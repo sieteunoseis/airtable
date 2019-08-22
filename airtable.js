b1668a1abc4de75087ac3dd7c44e018cbd316794
Jeremy Worden <jeremy.worden@gmail.com>
2019-08-22 13:19:54 -0700

    Add files via upload

---
+++ airtable.js
@@ -0,0 +1,23 @@
+var Airtable = require('airtable');
+
+module.exports = (atapikey, atbaseid, attablename, atviewname) => {
+	const atservice = {};
+	atservice.getRecords = () => {
+		return new Promise((resolve, reject) => {
+			var base = new Airtable({apiKey: atapikey}).base(atbaseid);
+			var totalrecords = [];
+			base(attablename).select({
+				view: atviewname
+			}).eachPage(function page(records, fetchNextPage) {
+				// This function (`page`) will get called for each page of records.
+				totalrecords.push(records);
+				fetchNextPage();
+
+			}, function done(err) {
+				if (err) { reject(err); return; }
+				resolve(totalrecords);
+			});
+		});    
+	};
+	return atservice;
+};
